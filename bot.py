4810def0dce8dc8f2bce69c450bd77674159cfde
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-15 14:06:29 +0200

    komenda clear i nakedcowboys

--- bot.py
+++ bot.py
@@ -9,11 +9,19 @@ async def on_ready ():
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} zawitał/a na Ram Ranch!')
+    await member.send(f'{member} zawitał/a na Ram Ranch!')
 
 @client.event
 async def on_member_remove(member):
     print(f'{member} opuścił/a Ram Ranch!')
+
+@client.command()   
+async def NakedCowboys(member):
+    print('at Ram Ranch')
+
+@client.command()   
+async def clear(member, ile = 4 ):
+    await member.channel.purge(limit = ile)#mem.channel zwraca obj channel
 
 token=open("token.txt")#plik z tokenem
 client.run(token.read())
